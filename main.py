30eb7c12eb85eefe9675a449187c6e111362010b
Tanuj22 <agarwal.5@iitj.ac.in>
2020-10-20 02:14:29 +0530

    same data as csv and plot
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
+import csv
+import os
 import json
+import numpy as np
 from rastermap import RasterMap
 import algorithms as algo
+import matplotlib.pyplot as plt
 
 args = json.load(open('args.json'))
 
 current_map = RasterMap(args['tif'])
 
-
 algorithms = {
-    'Gradient Decent' : {'function' : }
+    'Gradient Decent': {'function': algo.gradient_descent, 'color': '#FF0000'}
 }
+
+plt.style.use('ggplot')
+fig = plt.figure()
+
+for k, v in algorithms.items():
+    print(f"\n{'-' * 10} {k} {'-' * 10}")
+    theta, j_history = v['function'](current_map, np.array([args['center']['lat'],
+                                                            args['center']['lng']]),
+                                     num_iters=args['iters'])
+    with open(f'{args["output"]}{"-".join(k.strip().split())}.csv',
+              'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([v['color']])
+        for weight in j_history:
+            writer.writerow([weight[1], weight[2]])
+    plt.plot(range(j_history.shape[0]), j_history[:, 0], label=k, c=v['color'])
+
+plt.xlabel('Iterations')
+plt.ylabel('Elevation')
+plt.title('Hill Climbing Algorithms')
+plt.legend()
+plt.tight_layout()
+
+# save the plot
+fig.savefig('Cost_Plot.png', dpi=fig.dpi)
+plt.show()
