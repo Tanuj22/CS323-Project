c38e8b63aea0844a303d2da42503258b5d1139a6
Tanuj22 <agarwal.5@iitj.ac.in>
2020-10-24 22:32:42 +0530

    Stochastic hill climbing
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,5 +1,6 @@
+import random
 import numpy as np
-from .helpers import calculate_gradient, get_possible_steps
+from .helpers import calculate_gradient, get_possible_steps, get_step_costs
 
 
 def tabu_search(rmap, theta, tabu_size=10, num_iters=10000):
@@ -32,6 +33,33 @@ def tabu_search(rmap, theta, tabu_size=10, num_iters=10000):
         print(f'({i}/{num_iters}): Update is {best_t}')
 
     return best_t, j_history[:i]
+
+
+def stochastic_hill_climb(rmap, theta, num_iters=10000):
+    cost = rmap.get_cost(*theta)
+    elevation = rmap.get_elevation(*theta)
+    j_history = np.zeros(shape=(num_iters, 3))
+    j_history[0] = [elevation, theta[0], theta[1]]
+
+    for i in range(1, num_iters):
+        steps = get_possible_steps(theta)
+        step_costs = get_step_costs(rmap, steps)
+
+        # extra loop for randomness to settle in
+        for j in range(50):
+            step, step_cost = random.choice(list(zip(steps, step_costs)))
+
+            if step_cost <= cost:
+                theta = step
+                cost = step_cost
+
+        elevation = rmap.get_elevation(*theta)
+        j_history[i] = [elevation, theta[0], theta[1]]
+
+        print(f'Elevation at {theta} is {elevation}')
+        print(f'({i}/{num_iters}): Update is {theta}')
+
+    return theta, j_history[:i]
 
 
 def gradient_descent(rmap, theta, alpha=.01, num_iters=10000):
